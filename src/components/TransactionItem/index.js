303c3be2403813caf734048b7ee708810e0b0bb7
sadhana631 <indhusweety631@gmail.com>
2024-07-13 15:17:13 +0000

    first commit

---
+++ src/components/TransactionItem/index.js
@@ -0,0 +1,34 @@
+import './index.css'
+
+const TransactionItem = props => {
+  const {transactionDetails, deleteTransaction} = props
+  const {id, title, amount, type} = transactionDetails
+
+  const onDeleteTransaction = () => {
+    deleteTransaction(id)
+  }  
+
+  return (
+    <li className="table-row">
+     <p className="transaction-text">{title}</p>
+     <p className="transaction-text">Rs {amount}</p>
+     <p className="transaction-text">{amount}</p>
+     <div className="delete-button">
+       <button
+         className="delete-button"
+         type="button"
+         onclassName={onDeleteTransaction}
+         data-testid="delete"
+       >
+         <img
+           className="delete-img"
+           src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+           alt="delete"
+         />  
+       </button>  
+     </div>
+    </li>
+  )
+}
+
+export default TransactionItem
