6ccd765bab93c74b98ce92fed9368a7abae06a45
sadhana631 <indhusweety631@gmail.com>
2024-07-15 07:18:21 +0000

    second commit

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -6,27 +6,27 @@ const TransactionItem = props => {
 
   const onDeleteTransaction = () => {
     deleteTransaction(id)
-  }  
+  }
 
   return (
     <li className="table-row">
-     <p className="transaction-text">{title}</p>
-     <p className="transaction-text">Rs {amount}</p>
-     <p className="transaction-text">{amount}</p>
-     <div className="delete-button">
-       <button
-         className="delete-button"
-         type="button"
-         onclassName={onDeleteTransaction}
-         data-testid="delete"
-       >
-         <img
-           className="delete-img"
-           src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
-           alt="delete"
-         />  
-       </button>  
-     </div>
+      <p className="transaction-text">{title}</p>
+      <p className="transaction-text">Rs {amount}</p>
+      <p className="transaction-text">{amount}</p>
+      <div className="delete-button">
+        <button
+          className="delete-button"
+          type="button"
+          onclassName={onDeleteTransaction}
+          data-testid="delete"
+        >
+          <img
+            className="delete-img"
+            src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+            alt="delete"
+          />
+        </button>
+      </div>
     </li>
   )
 }
